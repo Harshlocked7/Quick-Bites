66d930ef5c988c85274ba4d43b8980b05f48e7f5
harshlocked7 <harshlocked7@gmail.com>
2024-02-21 23:38:07 +0530

    1

--- Quick-Bites-Client/src/components/FoodCard.jsx
+++ Quick-Bites-Client/src/components/FoodCard.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { FaStar } from "react-icons/fa";
 
 
-const FoodCard = () => {
+const FoodCard = ({id, name, price, desc, img, rat}) => {
   return (
-    <div className="font-bold w-[250px]  rounded-lg p-3 flex flex-col hover:scale-110 cursor-grab transiton-all duration-500 ease-in-out gap-2">
-      <img src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328
-      ~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8b
-      b6b875a78a1eda09cb5" alt="" 
+    <div className="font-bold w-[250px] p-3 flex flex-col hover:scale-110 cursor-grab transiton-all duration-500 ease-in-out gap-1 rounded-lg">
+      <img src={img} alt="" 
       className="w-auto h-[140px] "/>
-      <div className=' bg-yellow-300 p-2 '>
+      <div className=' bg-yellow-300 p-2 rounded-lg'>
         <div className='text-sm flex justify-between'>
         <h2>Onion Pizza</h2>
         <span className='text-green-600'>₹ 180</span>
--- Quick-Bites-Client/src/components/FoodItems.jsx
+++ Quick-Bites-Client/src/components/FoodItems.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import FoodCard from './FoodCard';
 // import FoodData from './data/FoodData.js';
-import FoodData from '../data/FoodData'
+// import FoodData from '../data/FoodData'
+// import FoodData from '../data/foodData.js';
+import food1 from '../food.js'
 
 const FoodItems = () => {
   return (
-    <div>
-        {FoodData.map((food) => { 
-            return <FoodCard key={food.id}/>})}
-    </div>
-  )
+    <>
+        {food1.map((food) => { 
+            return (
+            <FoodCard 
+            key={food.id} id={food.id} name={food.name} price={food.price} 
+            desc={food.desc}
+            rating={food.rating}
+            img={food.img}/>
+        );
+        })}
+        
+    </>
+  );
 }
 
 export default FoodItems;
