6299d87de872c7ceaeebf4fe6dd07e74974165b8
harshlocked7 <harshlocked7@gmail.com>
2024-02-24 18:04:43 +0530

    Category filter

---
+++ Quick-Bites-Client/src/redux/slices/CategorySlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const CategorySlice = createSlice ({
+    name : "category",
+    initialState : {
+        category: "ALL",
+    },
+    reducers: {
+        setCategory: (state, action) => {
+            state.category = action.payload;
+        }
+    },
+}) 
