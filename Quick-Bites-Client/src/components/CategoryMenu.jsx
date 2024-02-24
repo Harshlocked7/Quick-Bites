1a95dd9613ec54a6f7ba32ad904185210201393e
harshlocked7 <harshlocked7@gmail.com>
2024-02-24 18:06:55 +0530

    Cart menu

--- Quick-Bites-Client/src/components/CategoryMenu.jsx
+++ Quick-Bites-Client/src/components/CategoryMenu.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import food from '../food.js'
 
 const CategoryMenu = () => {
+  const [categories, setCategories] = useState([]);
+  const listUniqueCategory = () => {
+    const uniqueCategories = [
+      ...new Set(food.map((food) => food.category))];
+      setCategories(uniqueCategories)
+  }
+  useEffect(() => {
+    listUniqueCategory();
+  }, [])
   return (
     <div className='mx-6 '>
        <h3 className='text-xl font-semibold '> Find the best Food </h3>
        <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth '>
-       <button className='bg-yellow-300 px-3 py-2 bg-gray-200 font-bold hover rounded-lg hover:bg-green-500 hover:text-white'>
-        All
+       
+       {
+        categories.map((category, index) => {
+          return (
+            <button key={index} className='bg-yellow-300 px-3 py-2 bg-gray-200 font-bold hover rounded-lg hover:bg-green-500 hover:text-white'>
+              {category}
        </button>
-       <button className='bg-yellow-300 px-3 py-2 bg-gray-200 font-bold hover rounded-lg hover:bg-green-500 hover:text-white'>
-        BreakFast
-       </button>
-       <button className='bg-yellow-300 px-3 py-2  font-bold hover rounded-lg hover:bg-green-500 hover:text-white'>
-        Lunch
-       </button>
-       <button className='bg-yellow-300 px-3 py-2 bg-gray-200 font-bold hover rounded-lg hover:bg-green-500 hover:text-white'>
-        Dinner
-       </button>
-       <button className='bg-yellow-300 px-3 py-2 bg-gray-200 font-bold hover rounded-lg hover:bg-green-500 hover:text-white'>
-        Snacks
-       </button>
+          );
+        })
+       }
        </div>
     </div>
   )
