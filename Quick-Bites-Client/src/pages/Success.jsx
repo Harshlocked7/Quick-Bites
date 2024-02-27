6ed09435d8a322fe1ba54f6e6a6d98101beb122d
harshlocked7 <harshlocked7@gmail.com>
2024-02-27 17:23:48 +0530

    Success page

--- Quick-Bites-Client/src/pages/Success.jsx
+++ Quick-Bites-Client/src/pages/Success.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { PropagateLoader } from "react-spinners"
 
 
 const Success = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect (() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000)
+  }, [])
   return (
-    <div>Success</div>
+    <div className='flex flex-col  items-center justify-center h-screen'>
+      { loading ? ( <PropagateLoader color="#36d7b7" /> ) : (<div> <h2 className='text-3xl font-semibold mb-4 text-center'>
+        Order Successful!
+      </h2>
+      <p>Your order has been succesfully placed</p> </div>)
+      }
+     
+    </div>
   )
 }
 
