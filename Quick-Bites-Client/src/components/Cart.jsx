622040b5ec5587ad68fa8babf661f86d69eceb4c
harshlocked7 <harshlocked7@gmail.com>
2024-02-27 17:21:32 +0530

    Navigation

--- Quick-Bites-Client/src/components/Cart.jsx
+++ Quick-Bites-Client/src/components/Cart.jsx
@@ -3,6 +3,7 @@ import { MdClose } from "react-icons/md";
 import ItemCard from './ItemCard';
 import { useSelector } from 'react-redux'; 
 import { MdShoppingCart } from "react-icons/md";
+import { useNavigate } from "react-router-dom"
 
 
 
@@ -11,6 +12,7 @@ const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cart);
   const totalQty = cartItems.reduce((totalQty, item) => totalQty + item.qty, 0);
   const totalPrice = cartItems.reduce((total, item) => total + item.qty * item.price, 0);
+  const navigate = useNavigate();
   return (
     <>
     <div className={`fixed right-0 top-0 w-full lg:w-[20vw] h-full bg-white p-5 ${activeCart ? "translate-x-0" : "translate-x-full"} transition-all duration-500 z-50`}>
@@ -35,7 +37,7 @@ const Cart = () => {
         <h3 className='font-semibold text-gray-800'>Items : {totalQty}</h3>
         <h3 className='font-semibold text-gray-800'>Total Amount : {totalPrice} </h3>
         <hr className='w-[90vw] lg:18vw my-2'/>
-        <button className='bg-green-500 font-bold p-3 text-white py-2 rounded-lg w-[90] lg:w-[18vw]'>Checkout</button>
+        <button onClick={()=> navigate("/success")} className='bg-green-500 font-bold p-3 text-white py-2 rounded-lg w-[90] lg:w-[18vw]'>Checkout</button>
     </div>
     
     </div>
