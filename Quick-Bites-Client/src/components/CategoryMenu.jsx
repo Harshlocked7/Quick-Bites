b92b882db46d8a3c617552dea9dd8a7c3e8d4f13
harshlocked7 <harshlocked7@gmail.com>
2024-02-27 17:21:51 +0530

    Category search filter

--- Quick-Bites-Client/src/components/CategoryMenu.jsx
+++ Quick-Bites-Client/src/components/CategoryMenu.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import food from '../food.js'
+import { useDispatch, useSelector } from "react-redux"
+import { setCategory } from '../redux/slices/CategorySlice.jsx'
 
 const CategoryMenu = () => {
   const [categories, setCategories] = useState([]);
@@ -11,15 +13,24 @@ const CategoryMenu = () => {
   useEffect(() => {
     listUniqueCategory();
   }, [])
+
+  const dispatch = useDispatch();
+  const selectedCategory = useSelector((state) => state.category.category)
+
   return (
     <div className='mx-6 '>
        <h3 className='text-xl font-semibold '> Find the best Food </h3>
-       <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth '>
-       
+       <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth'>
+       <button onClick={() => dispatch(setCategory("All"))}  className={`px-3 py-2 bg-yellow-300 font-bold hover:bg-green-500 rounded-lg  hover:text-white 
+       ${selectedCategory === "All" && "bg-green-600 text-white"}`}>
+              All
+       </button>
        {
         categories.map((category, index) => {
           return (
-            <button key={index} className='bg-yellow-300 px-3 py-2 bg-gray-200 font-bold hover rounded-lg hover:bg-green-500 hover:text-white'>
+            <button onClick={() => dispatch(setCategory(category))} key={index} 
+            className={` px-3 py-2 bg-yellow-300 font-bold hover rounded-lg hover:bg-green-500 hover:text-white 
+            ${selectedCategory === category && "bg-green-600 text-white"}`}>
               {category}
        </button>
           );
